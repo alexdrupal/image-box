dece532fa8f069ec36d0b1672096fa3570d7af68
pableke <pableke@gmail.com>
2017-03-01 00:22:06 +0100

    Desactive keyup event in popup

--- image-box.js
+++ image-box.js
@@ -1,5 +1,6 @@
 
 (function($) {
+	var index = -1; //position indicator
 	var settings = { //global config
 		duration: 300,
 		preserveHeight: true,
@@ -17,9 +18,10 @@
 		classTitle: "iboxTitle"
 	};
 
-	function close() {
+	function close(fn) {
+		index = -1; //desactive keyup events
 		settings.onClose && settings.onClose(); //call onclose event
-		return !$(iboxOverlay).fadeOut(settings.duration);
+		return !$(iboxOverlay).fadeOut(settings.duration, fn);
 	};
 
 	//create once all DOM elements to overlay image-box
@@ -39,9 +41,8 @@
 
 	$(function() { document.body.appendChild(iboxOverlay); });
 	$(document).keyup(function(e) {
-		if (!$(iboxOverlay).is(":visible"))
-			return null; //inactive box
-		(e.keyCode == 27) && $(iboxOverlay).click(); //escape keycode = 27
+		if (index < 0) return null; //inactive box
+		(e.keyCode == 27) && close(); //escape keycode = 27
 		(e.keyCode == 37) && $(btnPrev).click(); //left keycode = 37
 		(e.keyCode == 39) && $(btnNext).click(); //right keycode = 39
 	});
@@ -55,8 +56,7 @@
 	};
 
 	$.imageBoxClose = function() {
-		settings.onClose && settings.onClose(); //call onclose event
-		$(iboxOverlay).fadeOut(settings.duration, function() {
+		close(function() {
 			$(btnClose).show(); $(btnPrev).show(); $(btnNext).show();
 			$(iboxBullets).show(); $(iboxTitle).show();
 			$(this).unbind("click").click(close);
@@ -64,12 +64,10 @@
 	};
 
 	$.fn.imagebox = function(opts) {
-		var index = 0; //position indicator
 		var self = this; //reference to jQuery list
 		opts && $.extend(settings, opts); //inicialize settings
 		function range(val, min, max) { return Math.min(Math.max(val, min), max); };
 		function rangeList(val) { return range(val, 0, self.length - 1); };
-		function setIndex(j) { index = rangeList(j); return index; };
 		function onChange() {
 			if (settings.preserveHeight) {
 				var height = $(iboxImage).height() + "px";
@@ -80,15 +78,18 @@
 			settings.afterChange && settings.afterChange(); //call event
 		};
 
-		function getImage(i) { setIndex(i); return $(self[i]).attr("href"); };
+		function getImage(i) {
+			index = rangeList(i);
+			return $(self[i]).attr("href");
+		};
 		function setImage(i) {
 			$(btnPrev).hide(); $(btnNext).hide();
 			settings.beforeChange && settings.beforeChange(); //call event
 			$(iboxTitle).html($(self[i]).attr("title")); //set title image
-			$($("a", iboxBullets).removeClass(settings.classActiveBullet)[i])
-								.addClass(settings.classActiveBullet);
 			$(iboxImage).fadeOut(settings.duration, function() { //fade old image
 				$(this).attr("src", getImage(i)).fadeIn(settings.duration, onChange);
+				var bullets = $("a", iboxBullets).removeClass(settings.classActiveBullet);
+				$(bullets[index]).addClass(settings.classActiveBullet);
 			});
 			return false; //prevent event default for links
 		};
